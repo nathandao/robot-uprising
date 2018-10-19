035eef9b0b375db204813dd0c9b5762e01ddbb66
nathandao <nathan@guynathan.com>
2018-10-19 19:13:36 +0300

    update

--- speed_test.py
+++ speed_test.py
@@ -1,4 +1,4 @@
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank, MoveSteering
 
 wheel_circ = 3.14 * 5.6
 forward_distance = 150
@@ -7,10 +7,10 @@ left_distance = 100
 rotations1 = 150 / wheel_circ
 rotations2 = 100 / wheel_circ
 
+mt = MoveSteering(OUTPUT_A, OUTPUT_B)
 ms = MoveTank(OUTPUT_A, OUTPUT_B)
+
 ms.on_for_rotations(75, 75, rotations1)
-
-ms.on_for_degrees(-50, 50, 90)
-
+mt.on_for_rotations(-20, SpeedPercent(75), 10)
 ms.on_for_rotations(75, 75, rotations2)
 
