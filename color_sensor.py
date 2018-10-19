42dfd60eb78200debefe9a482a4da2cec3f936ef
nathandao <nathan@guynathan.com>
2018-10-19 20:15:27 +0300

    add color test

---
+++ color_sensor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from ev3dev2.sensor import ColorSensor, INPUT_1, INPUT_2
+
+cl = ColorSensor(INPUT_1)
+
+cl.mode = 'COL-COLOR'
+colors=('unknown','black','blue','green','yellow','red','white','brown')
+
+while True
+  print(colors[cl.value()])
