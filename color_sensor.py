4bbc7cf88d5abefc842684fade9b05782cf99e10
nathandao <nathan@guynathan.com>
2018-10-19 23:02:16 +0300

    add color sensors

--- color_sensor.py
+++ color_sensor.py
@@ -3,12 +3,18 @@
 from ev3dev2.sensor.lego import ColorSensor
 from ev3dev2.sensor import INPUT_1, INPUT_2
 
-cl1 = ColorSensor(INPUT_1)
-cl2 = ColorSensor(INPUT_2)
+class FoobotColorSensor:
+  def __init__(self):
+    self.left = ColorSensor(INPUT_1)
+    self.right = ColorSensor(INPUT_2)
 
-cl.mode = 'COL-REFLECT'
-colors=('unknown','black','blue','green','yellow','red','white','brown')
+  def get_sensor_color(self, s):
+    s.mode = 'COL-COLOR'
+    colors=('unknown','black','blue','green','yellow','red','white','brown')
+    return colors[s.value()]
 
-while True:
-  print(colors[cl1.value()])
-  print(colors[cl2.value()])
+  def left_color(self):
+    return self.get_sensor_color(self.left)
+
+  def right_color(self):
+    return self.get_sensor_right(self.right)
