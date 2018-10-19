e62ba7727830f11c5ae272587aeb02a8e77912b2
nathandao <nathan@guynathan.com>
2018-10-19 22:24:52 +0300

    add test lib

---
+++ lib/drive.py
@@ -0,0 +1,18 @@
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank, MoveSteering
+from math import pi
+
+class FoobotDrive:
+
+    def __init__(self, wheel_d, wheel_dist, default_speed = 50):
+        self.wheel_d = wheel_d
+        self.wheel_circ = wheel_d * pi
+        self.wheel_dist = wheel_dist
+        self.default_speed = default_speed
+        # self.move_steering = MoveSteering(OUTPUT_A, OUTPUT_B)
+        self.move_tank= MoveTank(OUTPUT_A, OUTPUT_B)
+
+    def move_straigth(self, distance, speed = 0):
+        if speed == 0:
+            speed = self.default_speed
+        rotations = distance / self.wheel_dist
+        self.move_tank.on_for_rotations(self.default_speed, self.default_speed, rotations)
--- speed_test.py
+++ speed_test.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank, MoveSteering
+# from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank, MoveSteering
 
-wheel_circ = 3.14 * 5.6
-forward_distance = 150
-left_distance = 100
+# wheel_circ = 3.14 * 5.6
+# forward_distance = 150
+# left_distance = 100
 
-rotations1 = 150 / wheel_circ
-rotations2 = 100 / wheel_circ
-mt = MoveSteering(OUTPUT_A, OUTPUT_B)
-ms = MoveTank(OUTPUT_A, OUTPUT_B)
+# rotations1 = 150 / wheel_circ
+# rotations2 = 100 / wheel_circ
+# mt = MoveSteering(OUTPUT_A, OUTPUT_B)
+# ms = MoveTank(OUTPUT_A, OUTPUT_B)
 
-ms.on_for_rotations(75, 75, rotations1)
-rotation_dist = 3 * 3.14
-ms.on_for_rotations(-25, 25, rotation_dist / wheel_circ)
-ms.on_for_rotations(75, 75, rotations2)
+# ms.on_for_rotations(75, 75, rotations1)
+# rotation_dist = 3 * 3.14
+# ms.on_for_rotations(-25, 25, rotation_dist / wheel_circ)
+# ms.on_for_rotations(75, 75, rotations2)
 
+from "lib/drive" import FoobotDrive
+
+FoobotDrive.move_straigth(10)
