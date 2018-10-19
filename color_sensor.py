c816bed95d656e89f7df55c3cfe7f83c727b79d1
nathandao <nathan@guynathan.com>
2018-10-19 20:48:06 +0300

    update

--- color_sensor.py
+++ color_sensor.py
@@ -3,10 +3,12 @@
 from ev3dev2.sensor.lego import ColorSensor
 from ev3dev2.sensor import INPUT_1, INPUT_2
 
-cl = ColorSensor(INPUT_1)
+cl1 = ColorSensor(INPUT_1)
+cl2 = ColorSensor(INPUT_2)
 
 cl.mode = 'COL-REFLECT'
 colors=('unknown','black','blue','green','yellow','red','white','brown')
 
 while True:
-  print(cl.value())
+  print(colors[cl1.value()])
+  print(colors[cl2.value()])
