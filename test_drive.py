5c3f45c01b93f13a8e007f5fdc42220e410b802e
nathandao <nathan@guynathan.com>
2018-10-21 02:15:07 +0300

    fix dimension

--- test_drive.py
+++ test_drive.py
@@ -4,6 +4,6 @@
 # sp.on_for_rotations(-100, SpeedPercent(20), 10)
 
 from foobot.drive import FoobotDrive
-car = FoobotDrive(4.7, 11.4, 1, 70, 20)
+car = FoobotDrive(4.7, 11.1, 1, 70, 20)
 car.turn_left(45)
 car.turn_right(45)
