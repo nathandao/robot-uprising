cbc479fd40a7adea8765daaf07a4673cf131ab85
Panttu <panttu429@gmail.com>
2018-10-20 22:00:02 +0300

    added variables to own file

---
+++ variables.py
@@ -0,0 +1,18 @@
+
+whiteRMinVal = 200
+whiteBMinVal = 200
+whiteGMinVal = 200
+
+yellowRMinVal = 140
+yellowRMaxVal = 160
+yellowBMinVal = 140
+yellowBMaxVal = 160
+yellowGMinVal = 50
+yellowGMaxVal = 70
+
+orangeRMinVal = 155
+orangeRMaxVal = 180
+orangeBMinVal = 30
+orangeBMaxVal = 55
+orangeGMinVal = 20
+orangeGMaxVal = 45
