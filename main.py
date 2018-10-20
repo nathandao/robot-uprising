409148bff8d62954f6ba91a18af7e265edefa2ce
nathandao <nathan@guynathan.com>
2018-10-20 17:42:37 +0300

    fix color main

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ def t_cs_left():
             cs_left_rgb = cs.rgb_left()
         if cs_left_mode == 'ambient':
             cs_left_reading = cs.ambient_left()
+        sleep(0.1);
 
 cs_left_thread = Thread(target = t_cs_left)
 
@@ -45,9 +46,10 @@ def t_cs_right():
     global cs_right_mode, cs_right_reading, cs_right_rgb
     while loop:
         if cs_right_mode == 'rgb':
-            cs_right_rgb = cs.rgb_left()
+            cs_right_rgb = cs.rgb_right()
         if cs_left_mode == 'ambient':
-            cs_left_reading = cs.ambient_left()
+            cs_left_reading = cs.ambient_right()
+        sleep(0.1);
 
 cs_right_thread = Thread(target = t_cs_right)
 
@@ -55,6 +57,7 @@ def t_us():
     global us_distance
     while loop:
         us_distance = us.distance()
+        sleep(0.1);
 
 us_thread = Thread(target = t_us)
 
