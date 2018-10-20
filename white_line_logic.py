4a47f241502b55d7576e84968f28ceab3f0ea5d8
Panttu <panttu429@gmail.com>
2018-10-20 17:34:05 +0300

    Follows white line

---
+++ white_line_logic.py
@@ -0,0 +1,31 @@
+class WhiteLineFollower:
+    def __init__(self, car, colorsensor):
+        self.car = car
+        self.color_sensor  color_sensor
+
+    def isValueBetween(min, max, value):
+        if value < min and value > max:
+            return True
+        return False
+
+    def is_it_white(rbg):
+        if rbg(0) > 200 and rgb(1) > 200 and rgb(2) > 200:
+            return True
+        return False
+
+    def is_it_yellow(rbg):
+        if isValueBetween(140, 160, rbg(0)) and isValueBetween(140, 160, rbg(0)) and isValueBetween(50, 70, rbg(0)):
+            return True
+        return False
+    
+    def run():
+        while True:
+            if is_it_white(self.colorsensor.rgb_left()):
+                self.car.turn_left()
+            elif is_it_white(self.colorsensor.rgb_right()):
+                self.car.turn_right()
+            else:
+                self.car.run()
+    def stop():
+        self.car.stop
+
