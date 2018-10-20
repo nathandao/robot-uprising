72f4e66748e0d488453b942b6c16d40ed2da833a
Panttu <panttu429@gmail.com>
2018-10-21 01:00:13 +0300

    color fix

--- variables.py
+++ variables.py
@@ -1,18 +1,20 @@
 
 whiteRMinVal = 200
-whiteBMinVal = 200
 whiteGMinVal = 200
+whiteBMinVal = 200
 
 yellowRMinVal = 140
 yellowRMaxVal = 160
-yellowBMinVal = 140
-yellowBMaxVal = 160
-yellowGMinVal = 50
-yellowGMaxVal = 70
+yellowGMinVal = 140
+yellowGMaxVal = 160
+yellowBMinVal = 50
+yellowBMaxVal = 170
+
 
 orangeRMinVal = 155
 orangeRMaxVal = 180
-orangeBMinVal = 30
-orangeBMaxVal = 55
-orangeGMinVal = 20
-orangeGMaxVal = 45
+orangeGMinVal = 30
+orangeGMaxVal = 55
+orangeBMinVal = 20
+orangeBMaxVal = 45
+
--- white_line_logic.py
+++ white_line_logic.py
@@ -12,26 +12,26 @@ class WhiteLineFollower:
         return False
 
     def is_it_white(self, rbg):
-        if rbg[0] > whiteRMinVal and rgb[1] > whiteBMinVal and rgb[2] > whiteGMinVal:
+        if rbg[0] > whiteRMinVal and rgb[1] > whiteGMinVal and rgb[2] > whiteBMinVal:
             print('is WHITE')
             return True
         print('NOT ----- white')
         return False
 
     def is_it_yellow(self, rbg):
-        if self.isValueBetween(yellowRMinVal, yellowRMaxVal, rbg[0]) and self.isValueBetween(yellowBMinVal, yellowBMaxVal, rbg[1]) and self.isValueBetween(yellowGMinVal, yellowGMaxVal, rbg[2]):
+        if self.isValueBetween(yellowRMinVal, yellowRMaxVal, rbg[0]) and self.isValueBetween(yellowGMinVal, yellowGMaxVal, rbg[1]) and self.isValueBetween(yellowBMinVal, yellowBMaxVal, rbg[2]):
             return True
         return False
 
     def is_it_orange(self, rbg):
-        if self.isValueBetween(orangeRMinVal, orangeRMaxVal, rbg[0]) and self.isValueBetween(orangeBMinVal, orangeBMaxVal, rbg[1]) and self.isValueBetween(orangeGMinVal, orangeGMaxVal, rbg[2]):
+        if self.isValueBetween(orangeRMinVal, orangeRMaxVal, rbg[0]) and self.isValueBetween(orangeGMinVal, orangeGMaxVal, rbg[1]) and self.isValueBetween(orangeBMinVal, orangeBMaxVal, rbg[2]):
             return True
         return False
 
     def run(self):
         while True:
             # If both sensors on white, prioritise right turns
-            if self.is_it_white(self.color_sensor.rgb_left()) and self.is_it_white(self.color_sensor.rbg_right()):
+            if self.is_it_white(self.color_sensor.rgb_left()) and self.is_it_white(self.color_sensor.rgb_right()):
                 self.car.stop()
                 self.car.turn_right(15)
             elif self.is_it_white(self.color_sensor.rgb_left()):
@@ -41,7 +41,7 @@ class WhiteLineFollower:
                 self.car.stop()
                 self.car.turn_right(15)
             # On yellow just go pres button and come back
-            elif self.is_it_yellow(self.color_sensor.rgb_left()) or self.is_it_yellow(self.color_sensor.rbg_right()):
+            elif self.is_it_yellow(self.color_sensor.rgb_left()) or self.is_it_yellow(self.color_sensor.rgb_right()):
                 print('YELLOW ---')
                 self.car.stop()
                 self.car.move_straigth(10)
@@ -49,7 +49,7 @@ class WhiteLineFollower:
                 self.car.move_straigth(-10)
                 self.car.turn_left(180)
             # Section end if we reach orange color
-            elif self.is_it_orange(self.color_sensor.rgb_left()) or self.is_it_orange(self.color_sensor.rbg_right()):
+            elif self.is_it_orange(self.color_sensor.rgb_left()) or self.is_it_orange(self.color_sensor.rgb_right()):
                 self.car.stop()
             else:
                 print('RUN FORWARD ---')
