b1714cf6c8705d63b9d86e7a262d392293f6041d
nathandao <nathan@guynathan.com>
2018-10-21 01:16:02 +0300

    test

--- test_drive.py
+++ test_drive.py
@@ -1,8 +1,9 @@
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank, MoveSteering
 
-from foobot.drive import FoobotDrive
+sp = MoveSteering(OUTPUT_A, OUTPUT_B)
+sp.on_for_rotations(-50, SpeedPercent(75), 45)
 
-car = FoobotDrive(4.7, 11.4, 1, 70, 20)
-
-car.turn(45)
-car.turn(-45)
+# from foobot.drive import FoobotDrive
+# car = FoobotDrive(4.7, 11.4, 1, 70, 20)
+# car.turn(-45)
+# car.turn(45)
