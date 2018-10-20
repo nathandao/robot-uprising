86a36baf0fd83b9abe45bb9bf4190b5198dfa4cc
nathandao <nathan@guynathan.com>
2018-10-20 13:34:32 +0300

    update

--- color_test.py
+++ color_test.py
@@ -3,26 +3,35 @@ from foobot.color_sensor import FoobotColorSensor
 color_sensor = FoobotColorSensor()
 
 while True:
+    side = input('left, right: ')
     m = input('color , intensity, ambient, rgb: ')
     if m == 'color':
         while True:
-            print(color_sensor.color_left())
-            print(color_sensor.color_right())
+            if side == 'left':
+                print(color_sensor.color_left())
+            else:
+                print(color_sensor.color_right())
 
     if m == 'intensity':
         while True:
-            print(color_sensor.intensity_left())
-            print(color_sensor.intensity_right())
+            if side == 'left':
+                print(color_sensor.intensity_left())
+            else:
+                print(color_sensor.intensity_right())
 
     if m == 'ambient':
         while True:
-            print(color_sensor.intensity_left())
-            print(color_sensor.intensity_right())
+            if side == 'left':
+                print(color_sensor.intensity_left())
+            else:
+                print(color_sensor.intensity_right())
 
     if m == 'rgd':
         while True:
-            print(','.join(str(x) for x in color_sensor.rgb_left()))
-            print(','.join(str(x) for x in color_sensor.rgb_right()))
+            if side == 'left':
+                print(','.join(str(x) for x in color_sensor.rgb_left()))
+            else:
+                print(','.join(str(x) for x in color_sensor.rgb_right()))
 
 # print(color_sensor.color_left())
 # print("intensity")
