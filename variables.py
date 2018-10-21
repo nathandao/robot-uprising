26fc75d1457ad0d9c7746995d4e39daf613b8757
Olli Ohls <Uulari@gmail.com>
2018-10-21 05:10:11 +0300

    added many variables with high and low values and a thresold for it

--- variables.py
+++ variables.py
@@ -1,3 +1,87 @@
+threshold = 20
+
+grey_shadow_high_r = (31 + 42 / 2) + threshold
+grey_shadow_high_g = (36 + 53 / 2) + threshold
+grey_shadow_high_b = (41 + 51 / 2) + threshold
+grey_shadow_low_r = (31 + 42 / 2) - threshold
+grey_shadow_low_g = (36 + 53 / 2) - threshold
+grey_shadow_low_b = (41 + 51 / 2) - threshold
+
+
+grey_medshadow_high_r = (33 + 42 / 2) + threshold
+grey_medshadow_high_g = (38 + 55 / 2) + threshold
+grey_medshadow_high_b = (44 + 52 / 2) + threshold
+grey_medshadow_low_r = (33 + 42 / 2) - threshold
+grey_medshadow_low_g = (38 + 55 / 2) - threshold
+grey_medshadow_low_b = (44 + 52 / 2) - threshold
+
+grey_light_high_r = (33 + 42 / 2) + threshold
+grey_light_high_g = (37 + 54 / 2) + threshold
+grey_light_high_b = (41 + 52 / 2) + threshold
+grey_light_low_r = (33 + 42 / 2) - threshold
+grey_light_low_g = (37 + 54 / 2) - threshold
+grey_light_low_b = (41 + 52 / 2) - threshold
+
+
+orangetape_shadow_high_r = (166 + 205 / 2) + threshold
+orangetape_shadow_high_g = (41 + 59 / 2) + threshold
+orangetape_shadow_high_b = (33 + 40 / 2) + threshold
+
+orangetape_shadow_low_r = (166 + 205 / 2) - threshold
+orangetape_shadow_low_g = (41 + 59 / 2)  - threshold
+orangetape_shadow_low_b = (33 + 40 / 2) - threshold
+
+orangetape_light_high_r = (168 + 212 / 2) + threshold
+orangetape_light_high_g = (44 + 61 / 2) + threshold
+orangetape_light_high_b = (34 + 40 / 2) + threshold
+
+orangetape_light_low_r = (168 + 212 / 2) - threshold
+orangetape_light_low_g = (44 + 61 / 2) - threshold
+orangetape_light_low_b = (34 + 40 / 2) - threshold
+
+orange_shadow_high_r = (177 + 204 / 2) + threshold
+orange_shadow_high_g = (205 + 225 / 2) + threshold
+orange_shadow_high_b = (338 + 280 / 2) + threshold
+
+orange_shadow_low_r = (177 + 204 / 2) - threshold
+orange_shadow_low_g = (205 + 225 / 2) - threshold
+orange_shadow_low_b = (338 + 280 / 2) - threshold
+
+orange_light_high_r = (173 + 211 / 2) + threshold
+orange_light_high_g = (198 + 234 / 2) + threshold
+orange_light_high_b = (330 + 282 / 2) + threshold
+
+orange_light_high_r = (173 + 211 / 2) - threshold
+orange_light_high_g = (198 + 234 / 2) - threshold
+orange_light_high_b = (330 + 282 / 2) - threshold
+
+
+whitetape_shadow_high_r = (171 + 207 / 2) + threshold
+whitetape_shadow_high_g = (108 + 262 / 2) + threshold
+whitetape_shadow_high_b = (212 + 247 / 2) + threshold
+
+whitetape_shadow_low_r = (171 + 207 / 2) - threshold
+whitetape_shadow_low_g = (108 + 262 / 2) - threshold
+whitetape_shadow_low_b = (212 + 247 / 2) - threshold
+
+whitetape_light_high_r = (169 + 222 / 2) + threshold
+whitetape_light_high_g = (189 + 278 / 2) + threshold
+whitetape_light_high_b = (215 + 264 / 2) + threshold
+
+whitetape_light_low_r = (169 + 222 / 2) - threshold
+whitetape_light_low_g = (189 + 278 / 2) - threshold
+whitetape_light_low_b = (215 + 264 / 2) - threshold
+
+yellowpad_light_high_r = (143 + 200 / 2) + threshold
+yellowpad_light_high_g = (112 + 168 / 2) + threshold
+yellowpad_light_high_b = (39 + 37 / 2) + threshold
+
+yellowpad_light_low_r = (143 + 200 / 2) - threshold
+yellowpad_light_low_g = (112 + 168 / 2) - threshold
+yellowpad_light_low_b = (39 + 37 / 2) - threshold
+
+
+
 
 whiteRMinVal = 200
 whiteGMinVal = 200
